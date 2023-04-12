4c748d98567a4f473b4e4bc8122693978aa05a93
Amir <a.em@outlook.com>
2023-04-12 22:03:38 +0200

    1st and 2nd stage of the code

--- project.js
+++ project.js
@@ -5,3 +5,40 @@
 // check of the user won
 // give the user their winnings
 // play again
+
+const prompt = require('prompt-sync')();
+
+function deposit() {
+     while (true) {
+          const depositAmount = prompt('Enter a deposit amount: ');
+          const numberDepositAmount = parseFloat(depositAmount);
+
+          if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+               console.log('invalid deposit amount, try again!');
+          } else {
+               return numberDepositAmount;
+          }
+     }
+}
+function getNumberOfLines() {
+     while (true) {
+          const lines = prompt('Enter the number of lines to bet on (1-3): ');
+          const numberOfLines = parseFloat(lines);
+
+          if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+               console.log('invalid number of lines, try again!');
+          } else {
+               return numberOfLines;
+          }
+     }
+}
+
+
+
+
+
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+
+// deposit();
