020ff2f44f2f541df57ece7ce36f9aae7a51393b
Amir <a.em@outlook.com>
2023-04-12 19:24:51 +0200

    summary of  JavaScript

---
+++ project.js
@@ -0,0 +1,7 @@
+// deposit some money
+// determine number of lines to bet on
+// collect a bet amount
+// spin the slot machine
+// check of the user won
+// give the user their winnings
+// play again
