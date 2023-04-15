62c4131cda1887f49bb3f63ae5a9fca22dcb0fe8
Amir <a.em@outlook.com>
2023-04-15 18:23:20 +0200

    4th stage of the code (reels)

--- project.js
+++ project.js
@@ -71,12 +71,30 @@ function getBet(balance, lines) {
      }
 }
 
+function spin() {
+     const symbols = [];
+     for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+          for (let i = 0; i < count; i++) {
+               symbols.push(symbol);
+          }
+     }
 
-
-
+     const reels = [];
+     for (let i = 0; i < COLS; i++) {
+          reels.push([]);
+          const reelSymbols = [...symbols];
+          for (let j = 0; j < ROWS; j++) {
+               const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+               const selectedSymbol = reelSymbols[randomIndex];
+               reels[i].push(selectedSymbol);
+               reelSymbols.splice(randomIndex, 1);
+          }
+     }
+     return reels;
+};
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
-
+const reels = spin();
 // deposit();
