a73d5c43c61ad6c71aef625910958ddd88922d24
Amir <a.em@outlook.com>
2023-04-15 18:50:06 +0200

    5th stage of the project (if user won)

--- project.js
+++ project.js
@@ -93,8 +93,64 @@ function spin() {
      return reels;
 };
 
+const transpose = (reels) => {
+     const rows = [];
+
+     for (let i = 0; i < ROWS; i++) {
+          rows.push([]);
+          for (let j = 0; j < COLS; j++) {
+               rows[i].push(reels[j][i]);
+          }
+     }
+
+     return rows;
+};
+
+const printRows = (rows) => {
+     for (const row of rows) {
+          let rowString = "";
+          for (const [i, symbol] of row.entries()) {
+               rowString += symbol;
+               if (i != row.length - 1) {
+                    rowString += " | ";
+               }
+          }
+          console.log(rowString);
+     }
+};
+
+const getWinnings = (rows, bet, lines) => {
+     let winnings = 0;
+
+     for (let row = 0; row < lines; row++) {
+          const symbols = rows[row];
+          let allSame = true;
+
+          for (const symbol of symbols) {
+               if (symbol != symbols[0]) {
+                    allSame = false;
+                    break;
+               }
+          }
+
+          if (allSame) {
+               winnings += bet * SYMBOL_VALUES[symbols[0]];
+          }
+     }
+
+     return winnings;
+};
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
+const winnings = getWinnings(rows, bet, numberOfLines);
+balance += winnings;
+console.log("You won, $" + winnings.toString());
+
+
+
 // deposit();
